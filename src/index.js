efd6e16e7be3e8c025addc1621c65a0bf0bf7f87
hemantlunia <149136418+hemantlunia@users.noreply.github.com>
2024-01-20 15:47:07 +0530

    utils discusssion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path:'./env'
@@ -10,7 +11,15 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8001, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+   console.log("MongDB connection fail(index.js root)",err);
+})
 
 
 
